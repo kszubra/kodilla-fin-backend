c2f0ae6573d6dd493a9a25ca1465482f22ef488e
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-04-26 15:25:50 +0200

    first commit

---
+++ src/main/java/com/kodilla/kodillafinalbackend/KodillaFinalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillafinalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaFinalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaFinalBackendApplication.class, args);
+    }
+
+}
