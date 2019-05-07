68aab0c55b16058e47930f884e2ba1ea0d909c5d
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-05-07 17:47:21 +0200

    update

--- src/test/java/com/kodilla/kodillafinalbackend/controler/UserControllerTest.java
+++ src/test/java/com/kodilla/kodillafinalbackend/controler/UserControllerTest.java
@@ -3,6 +3,7 @@ package com.kodilla.kodillafinalbackend.controler;
 import com.google.gson.Gson;
 import com.kodilla.kodillafinalbackend.domain.User;
 import com.kodilla.kodillafinalbackend.domain.UserDto;
+import com.kodilla.kodillafinalbackend.domain.UserRegistrationDto;
 import com.kodilla.kodillafinalbackend.mapper.UserMapper;
 import com.kodilla.kodillafinalbackend.service.UserService;
 import org.junit.Test;
@@ -15,7 +16,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -24,7 +24,7 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -41,11 +41,69 @@ public class UserControllerTest {
     private Gson gson = new Gson();
 
     @Test
-    public void registerUser() {
+    public void registerUser() throws Exception {
+        User testUser = User.builder()
+                .id(1L)
+                .name("John")
+                .surname("Rambo")
+                .email("rambo@rambo.com")
+                .registered(LocalDate.parse("2019-05-07"))
+                .securePassword("123456789")
+                .notificationPreferences(new HashSet<>())
+                .build();
+        UserRegistrationDto dto = UserRegistrationDto.builder()
+                .name("John")
+                .surname("Rambo")
+                .email("rambo@rambo.com")
+                .securePassword("123456789")
+                .build();
+        when(userService.addUser(testUser)).thenReturn(testUser);
+        when(userMapper.mapRegistrationDtoToUser(dto)).thenReturn(testUser);
+        when(userService.getUserById(1L)).thenReturn(testUser);
+
+        String jsonContent = gson.toJson(dto);
+
+        // When & Then
+        mockMvc.perform(post("/users").contentType(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonContent))
+                .andExpect(status().isOk());
     }
 
     @Test
-    public void getUser() {
+    public void getUser() throws Exception {
+        //Given
+        User testUser = User.builder()
+                .id(1L)
+                .name("John")
+                .surname("Rambo")
+                .email("rambo@rambo.com")
+                .registered(LocalDate.parse("2019-05-07"))
+                .securePassword("123456789")
+                .notificationPreferences(new HashSet<>())
+                .build();
+        UserDto testDto = UserDto.builder()
+                .id(1L)
+                .name("John")
+                .surname("Rambo")
+                .email("rambo@rambo.com")
+                .registered(LocalDate.parse("2019-05-07"))
+                .securePassword("123456789")
+                .notificationIds(new HashSet<>())
+                .build();
+        when(userService.getUserById(1L)).thenReturn(testUser);
+        when(userMapper.mapToDto(testUser)).thenReturn(testDto);
+
+        // When & Then
+        mockMvc.perform(get("/users/1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("John")))
+                .andExpect(jsonPath("$.surname", is("Rambo")))
+                .andExpect(jsonPath("$.email", is("rambo@rambo.com")))
+                .andExpect(jsonPath("$.securePassword", is("123456789")));
+        verify(userService, times(1)).getUserById(1L);
+
     }
 
     @Test
@@ -103,10 +161,41 @@ public class UserControllerTest {
     }
 
     @Test
-    public void updateUser() {
+    public void updateUser() throws Exception {
+        User testUser = User.builder()
+                .id(1L)
+                .name("John")
+                .surname("Rambo")
+                .email("rambo@rambo.com")
+                .registered(LocalDate.parse("2019-05-07"))
+                .securePassword("123456789")
+                .notificationPreferences(new HashSet<>())
+                .build();
+        UserDto dto = UserDto.builder()
+                .id(1L)
+                .name("John EDITED")
+                .surname("Rambo 2")
+                .email("rambo@rambo.com")
+                .securePassword("123456789")
+                .build();
+        when(userService.getUserById(1L)).thenReturn(testUser);
+        when(userMapper.mapToUser(dto)).thenReturn(testUser);
+
+        String jsonContent = gson.toJson(dto);
+
+        // When & Then
+        mockMvc.perform(put("/users").contentType(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonContent))
+                .andExpect(status().isOk());
+        verify(userService, times(1)).getUserById(1L);
     }
 
     @Test
-    public void deleteUser() {
+    public void deleteUser() throws Exception {
+        //When & Then
+        mockMvc.perform(delete("/users/1"))
+                .andExpect(status().isOk());
+        verify(userService, times(1)).deleteUserById(1L);
     }
 }
